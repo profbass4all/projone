472f0495e760be8dd2d8f8987c13bb7a179f031d
Ibrahim Abass <profbass4allgmail.com>
2021-06-21 14:41:34 +0100

    new files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Course from './components/Course'
+
+function App() {
+  return (
+    <div>
+     
+        <Course />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Course.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Course() {
+    const course = {
+        id:1,
+        name: 'Half Stack Application Development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id:1
+            },
+            {
+                name: 'Using Props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a Component',
+                exercises: 19,
+                id:3
+            },
+            {
+                name: 'Redux',
+                exercises: 23,
+                id:4
+            }
+        ]
+    }
+
+    const tot = course.parts.reduce((b, y)=> b + y.exercises, 0)
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            <ul>
+                {course.parts.map(cox=> <li key={cox.id}><h4>{cox.name} {cox.exercises}</h4></li>)}
+            </ul>
+            <h6>Total: {tot} exercises</h6>
+        </div>
+    )
+}
+
+export default Course
